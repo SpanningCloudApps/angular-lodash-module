70d1b0c4729f53eba50f66cc81531251e7dae1bf
kmc059000 <kmc059000@gmail.com>
2014-03-13 15:25:15 -0500

    fixed adaptation to lodash

--- angular-lodash-module.js
+++ angular-lodash-module.js
@@ -1,8 +1,8 @@
 /**
- * Let's you use underscore as a service from a controller.
- * Got the idea from: http://stackoverflow.com/questions/14968297/use-underscore-inside-controllers
- * @author: Andres Aguilar https://github.com/andresesfm
+ * Let's you use lodash as a service from a controller.
+ * Adapted from https://github.com/andresesfm/angular-underscore-module
+ * Homepage https://github.com/kmc059000
  */
-angular.module('underscore', []).factory('_', function() {
-    return window._; // assumes underscore has already been loaded on the page
+angular.module('lodash', []).factory('_', function() {
+    return window._; // assumes lodash has already been loaded on the page
 });
